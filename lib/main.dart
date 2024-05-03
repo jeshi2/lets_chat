2ca43ca3206156c33a329ac9dd4cb3e4156937d7
jeshi2 <jamesvean19@gmail.com>
2024-05-03 16:55:49 +0300

    sign up login done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'services/auth/login_or_register.dart';
+import 'package:lets_chat/firebase_options.dart';
+import 'package:lets_chat/services/auth/auth_gate.dart';
+import 'package:lets_chat/services/auth/auth_service.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AuthService(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,9 +21,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
     );
   }
 }
---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthService extends ChangeNotifier {
+  // instnace of auth
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // sign user in
+  Future<UserCredential> signInWithEmailandPassword(
+      String email, String password) async {
+    try {
+      // sign in
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    }
+    // catch any error
+    on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
+  // sign user out
+}
